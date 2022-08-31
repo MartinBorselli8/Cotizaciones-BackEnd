dc00b9b9b6126d37b11bd44be164cd2a2583873d
mborselli <mborselli@e-partners.com.ar>
2022-08-31 10:46:29 -0300

    e

--- Cotizaciones-BackEnd/Startup.cs
+++ Cotizaciones-BackEnd/Startup.cs
@@ -56,12 +56,7 @@ namespace Cotizaciones_BackEnd
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //app.UseCors(options =>
-            //{
-            //    options.WithOrigins("https://e-quotesservices.herokuapp.com/");
-            //    options.AllowAnyMethod();
-            //    options.AllowAnyHeader();
-            //});
+            app.UseCors(_MyCors);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
