952fad7de4651722f9b2b960011f4a64ab86930e
mborselli <mborselli@e-partners.com.ar>
2022-08-31 10:08:08 -0300

    se agrego la politica de cors para publicado

--- Cotizaciones-BackEnd/Startup.cs
+++ Cotizaciones-BackEnd/Startup.cs
@@ -49,7 +49,7 @@ namespace Cotizaciones_BackEnd
         {
             app.UseCors(options =>
             {
-                options.WithOrigins("*");
+                options.WithOrigins("*", "https://e-quotesservices.herokuapp.com/");
                 options.AllowAnyMethod();
                 options.AllowAnyHeader();
             });
