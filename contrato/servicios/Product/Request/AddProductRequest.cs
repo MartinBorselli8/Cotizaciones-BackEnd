2d29ebae8316169804371e480df4a99574c686ca
LucasLahournere <lahournerelucas@gmail.com>
2022-09-27 10:24:11 -0300

    modificacion de algunos campos requeridos

--- contrato/servicios/Product/Request/AddProductRequest.cs
+++ contrato/servicios/Product/Request/AddProductRequest.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace contrato.servicios.Product.Request
 {
     public class AddProductRequest
     {
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public decimal UnitPrice { get; set; }
     }
 }
